13be6f2755524a20a2d52c9f0e4ae7d4e9c31f0b
med ali <khemirimohamedali7@gmail.com>
2023-02-25 01:04:37 +0100

    faq and opinions sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,14 @@ function hideMenu() {
 function showMenu() {
   navLinks.style.right = "0";
 }
+
+
+// FAQ SHOW ANSWER
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach((btn) => {
+	btn.addEventListener("click", (e) => {
+		const question = e.currentTarget.parentElement.parentElement;
+		question.classList.toggle("show-text");
+	});
+});
